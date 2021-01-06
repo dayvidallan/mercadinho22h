c76b1d33a1ea0e5b4abf586704ef29b790245665
unknown <dayvidallan@hotmail.com>
2021-01-06 16:59:58 -0300

    heroku14

--- versailles/configs/settings.py
+++ versailles/configs/settings.py
@@ -1,7 +1,7 @@
 import os
 from decouple import config, Csv
 from dj_database_url import parse as dburl
-from configs import DEFAULT_DATABASE_URL, DEFAULT_FROM_EMAIL, EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_PORT, EMAIL_USE_TLS
+from .configs import DEFAULT_DATABASE_URL, DEFAULT_FROM_EMAIL, EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_PORT, EMAIL_USE_TLS
 
 APP_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 PROJECT_ROOT = os.path.abspath(os.path.dirname(APP_ROOT))
